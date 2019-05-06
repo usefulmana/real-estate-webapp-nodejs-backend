5fed9d82ea87769e6f0bc32d1c9ebf2c18a9b72e
Alex Nguyen <alex.nguyen3141@gmail.com>
2019-05-06 17:15:52 +0700

    Minor update

--- src/models/property.model.js
+++ src/models/property.model.js
@@ -8,6 +8,22 @@ mongoose.connect(uri, () => {
   console.log('Successfully connect to database');
 });
 
+var addDaysToDate = (date, days) => {
+  return new Date(date.getTime() + days * 24 * 60 * 60000);
+};
+var DateFormat = date => {
+  var days = date.getDate();
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1;
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  var strTime = days + '/' + month + '/' + year + '/ ' + hours + ':' + minutes;
+  return strTime;
+};
+var now = new Date();
+var next = addDaysToDate(now, 90);
+
 const PropertySchema = new mongoose.Schema(
   {
     title: {
@@ -20,17 +36,26 @@ const PropertySchema = new mongoose.Schema(
     },
     area: Number,
     numOfBedrooms: Number,
+    numOfBathrooms: Number,
     direction: String,
     address: {
       type: String,
       require: true
     },
+    city: {
+      type: String,
+      require: true
+    },
+    province: {
+      type: String
+    },
     postDate: {
       type: Date,
-      default: Date.now()
+      default: now
     },
     endDate: {
-      type: Date
+      type: Date,
+      default: next
     },
     imageURL: [{ type: String }],
     project: { type: mongoose.Schema.Types.ObjectId, ref: 'projects' },
